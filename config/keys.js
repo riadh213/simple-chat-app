d24ede4873a62d93446ece9978e2a83073e0e2ac
riadhsss <saadiriadh767@gmail.com>
2025-01-18 21:30:39 +0100

    Initial commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://saadiriadh213:<db_password>@cluster0.th6cy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+module.exports = {
+    mongoURI: dbPassword
+};
